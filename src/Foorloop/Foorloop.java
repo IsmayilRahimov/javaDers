031ec7fa384c3379f8c94f715606fc2109c4cef4
IsmayilRahimov <rehimovismayil650@gmail.com>
2025-03-29 14:14:29 +0400

    de

---
+++ src/Foorloop/Foorloop.java
@@ -0,0 +1,15 @@
+package Foorloop;
+
+public class Foorloop {
+    public static void main(String[] args) {
+        for (double i = 0.5; i < 10; ++i) {
+
+            System.out.println(i+ "sayili addim . Ededimiz hal hazirda " + i+ "Buna beraberdir");
+
+        }
+
+    }
+
+
+    }
+
