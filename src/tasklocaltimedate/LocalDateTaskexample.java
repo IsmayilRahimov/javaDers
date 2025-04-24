5e7b8f39e4fd2a21e56a5d9a05b68ba5cc58d5b8
IsmayilRahimov <rehimovismayil650@gmail.com>
2025-04-24 21:32:23 +0400

    Ders task Reqemler

---
+++ src/tasklocaltimedate/LocalDateTaskexample.java
@@ -0,0 +1,19 @@
+package tasklocaltimedate;
+
+import java.time.LocalDate;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class LocalDateTaskexample {
+    public static void main(String[] args) {
+
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Tarix ve vaxt daxil edin:");
+        scanner.nextLine();
+
+
+
+
+    }
+}
