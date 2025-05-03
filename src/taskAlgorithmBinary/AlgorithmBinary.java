3d2125383b35b54c72811b477fa15ae6fcd9b004
IsmayilRahimov <rehimovismayil650@gmail.com>
2025-05-03 19:42:51 +0400

    ALGORITMLER UZRE TASKLAR

---
+++ src/taskAlgorithmBinary/AlgorithmBinary.java
@@ -0,0 +1,30 @@
+package taskAlgorithmBinary;
+
+public class AlgorithmBinary {
+    public static void main(String[] args) {
+        int[] arr = {0,1,3,5,7,9,11,13,15,17,19,21};
+        int target = 19;
+        int left =0, right =arr.length-1;
+        while  (left <= right){
+            int mid = (right + left)/2;
+            if (arr[mid] == target) {
+                System.out.println("Tapildi indeks: " + mid);
+                System.out.println("Eded ozu :" +arr[mid]);
+                break;
+            }
+                else if (arr[mid] < target) {
+                left = mid + 1;
+
+            }else {
+                    right= mid - 1;
+
+
+
+
+
+            }
+        }
+
+
+    }
+}
