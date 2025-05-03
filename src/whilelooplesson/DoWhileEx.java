2a8d494951c8bc554592af194233de2d208a7bed
IsmayilRahimov <rehimovismayil650@gmail.com>
2025-05-03 17:28:45 +0400

    Ozum ucun extestler:-

---
+++ src/whilelooplesson/DoWhileEx.java
@@ -0,0 +1,13 @@
+package whilelooplesson;
+
+public class DoWhileEx {
+    public static void main(String[] args) {
+        int i=1;
+        do{
+            System.out.println(i+ " olanda addim.-Ededimiz budur " +i);
+            i++;
+            System.out.println( "I -ni 1 vahid artirdiqda , kecirik novbeti defe " +i+  " ucun yoxlamaga:");
+
+        }while (i<10);
+    }
+}
---
+++ src/whilelooplesson/WhileexLoop.java
@@ -0,0 +1,13 @@
+package whilelooplesson;
+
+public class WhileexLoop {
+    public static void main(String[] args) {
+        int i = 0;
+        while ( i<=5 ){
+            System.out.println(i);
+            i++;
+            System.out.println(" I -ni 1 vahid artirdiqda , kecirik novbeti defe " +i+  " ucun yoxlamaga:" );
+
+        }
+    }
+}
